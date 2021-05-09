c111a0946bc449b0889c72f9a07d8d9fab38de2c
Huynh Duc Dung <dung@aircarbon.co>
2021-05-09 16:12:58 +0800

    feat: add header and footer components

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {}
+
+function Header({}: Props): ReactElement {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+        <Link href="/">
+          <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+            <Image width="30" height="30" src="/logo.png" />
+            <span className="ml-3 text-xl">Next Demo App</span>
+          </a>
+        </Link>
+        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
+          <Link href="/api-doc">
+            <a className="mr-5 hover:text-gray-900">API Doc</a>
+          </Link>
+          <Link href="/projects">
+            <a className="mr-5 hover:text-gray-900">Projects</a>
+          </Link>
+        </nav>
+        <a href="https://vercel.com/new/git/external?repository-url=https://github.com/jellydn/next-demo-app/">
+          <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
