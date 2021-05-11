f2350d2a43c5facfc113c2d3900597dc9f844a89
Huynh Duc Dung <dung@aircarbon.co>
2021-05-12 01:33:17 +0800

    :bug: Bad token format on github authorizatioin

--- services/github.ts
+++ services/github.ts
@@ -5,7 +5,7 @@ export async function getGithubUser(username: string) {
   const response = await fetch(`${GITHUB_URL}/users/${username}`, {
     method: "GET",
     headers: {
-      Authorization: process.env.GITHUB_TOKEN,
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
     },
   });
   if (response.ok) {
