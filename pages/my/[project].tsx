2031021c392ed91f9d6d466e44689817cd9b7998
Huynh Duc Dung <dung@aircarbon.co>
2021-05-23 14:40:16 +0800

    :sparkles: Show loading on fallback route

---
+++ pages/my/[project].tsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/prop-types */
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { useRouter } from "next/router";
+
+import Project from "../../components/Project";
+
+import {
+  getGithubUserRepositories,
+  getGithubUserRepository,
+} from "../../services/github";
+import { GithubProject } from "../../services/types";
+
+const MyProject = ({
+  repo,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <p>Loading...</p>;
+  }
+
+  return <Project repo={repo} />;
+};
+
+export async function getStaticPaths() {
+  const repositories = await getGithubUserRepositories(
+    process.env.NEXT_PUBLIC_GITHUB_USERNAME
+  );
+  return {
+    paths: repositories.map((project) => "/my/" + project.name),
+    fallback: true,
+  };
+}
+
+export const getStaticProps: GetStaticProps<{
+  repo: GithubProject;
+}> = async (ctx) => {
+  try {
+    const { params } = ctx;
+    const repo = await getGithubUserRepository(
+      process.env.NEXT_PUBLIC_GITHUB_USERNAME,
+      params.project as string
+    );
+
+    return {
+      props: {
+        repo,
+      },
+      revalidate: 1,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export default MyProject;
