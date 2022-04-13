1e0e630d1f67a0c56e15ba4873949de0023a007a
Huynh Duc Dung <dunghd.it@gmail.com>
2022-04-13 09:12:52 +0800

    :arrow_up: Update packages and fix build error with swagger-ui-react

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -1,5 +1,9 @@
-import SwaggerUI from "swagger-ui-react";
+import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
+
+const SwaggerUI = dynamic<{ url: string }>(import("swagger-ui-react"), {
+  ssr: false,
+});
 
 const ApiDoc = () => {
   return <SwaggerUI url="/api/doc" />;
