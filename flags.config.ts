bf221c6d417a5e70e99edde6a6eb2e82630b89b0
Huynh Duc Dung <dunghd.it@gmail.com>
2022-08-08 01:50:21 +0800

    fix(ci): read env file on build

--- flags.config.ts
+++ flags.config.ts
@@ -4,10 +4,12 @@ export type AppFlags = {
   "github-comment": boolean;
 };
 
+const FLAG_KEY =
+  process.env.NEXT_PUBLIC_FLAGS_ENVIRONMENT_KEY ??
+  "flags_pub_development_339304767359550036";
+
 configure<AppFlags>({
-  envKey:
-    process.env?.NEXT_PUBLIC_FLAGS_ENVIRONMENT_KEY ??
-    "flags_pub_development_339304767359550036",
+  envKey: FLAG_KEY,
   defaultFlags: {
     "github-comment": false,
   },
