607bdd2ca0c366481a6a43587a6605fe96fa2562
Huynh Duc Dung <dunghd.it@gmail.com>
2022-08-07 16:23:02 +0800

    :green_heart: add default value for happykit on dev

--- flags.config.ts
+++ flags.config.ts
@@ -5,7 +5,9 @@ export type AppFlags = {
 };
 
 configure<AppFlags>({
-  envKey: process.env.NEXT_PUBLIC_FLAGS_ENVIRONMENT_KEY,
+  envKey:
+    process.env?.NEXT_PUBLIC_FLAGS_ENVIRONMENT_KEY ??
+    "flags_pub_development_339304767359550036",
   defaultFlags: {
     "github-comment": false,
   },
