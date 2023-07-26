30458f9206ead15e406f2800fbd64475f44ee17f
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-26 23:34:40 +0800

    fix(eslint): conflict React rule

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
+  transpilePackages: [
+    'swagger-ui-react',
+    'swagger-client',
+    'react-syntax-highlighter',
+  ],
 };
--- pages/api/doc.ts
+++ pages/api/doc.ts
@@ -4,7 +4,8 @@ const swaggerHandler = withSwagger({
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "NextJS Swagger",
+      title: "NextJS Swagger Documentation",
+      description: "Swagger API Documentation for NextJS - https://github.com/jellydn/next-swagger-doc",
       version: "0.1.0",
     },
   },
