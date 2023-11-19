d97f5af646a914cadff78356ad352170efa48ff2
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-19 11:15:41 +0800

    fix: use Script for load script

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,23 @@
-import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 import React, { type ReactElement } from "react";
-
 
 function Header(): ReactElement {
   return (
     <header className="text-gray-600 body-font">
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5337133458846513"
-          crossOrigin="anonymous"
-         />
-      </Head>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5337133458846513"
+        crossOrigin="anonymous"
+      />
       <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
         <Link
           href="/"
-          className="flex items-center mb-4 font-medium text-gray-900 md:mb-0 title-font">
-
+          className="flex items-center mb-4 font-medium text-gray-900 md:mb-0 title-font"
+        >
           <Image alt="logo" width="30" height="30" src="/logo.png" />
           <span className="ml-3 text-xl">Next Demo App</span>
-
         </Link>
         <nav className="flex flex-wrap justify-center items-center text-base md:mr-auto md:ml-auto">
           {process.env.NODE_ENV === "development" && (
